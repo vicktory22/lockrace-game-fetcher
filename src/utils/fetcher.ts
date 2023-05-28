e083a68e0764948b018571b99649d71ad6977980
Tim Vick <t.vick@yahoo.com>
2023-05-27 18:55:33 -0700

    feat: add fetch with timeout

---
+++ src/utils/fetcher.ts
@@ -0,0 +1,22 @@
+type FetcherOptions = RequestInit & {
+  timeout?: number;
+};
+
+export const fetchWithTimeout = async (url: string, options: FetcherOptions = {}) => {
+  const { timeout = 800 } = options;
+
+  const controller = new AbortController();
+
+  const timeoutId = setTimeout(() => {
+    controller.abort();
+  }, timeout);
+
+  const response = await fetch(url, {
+    ...options,
+    signal: controller.signal,
+  });
+
+  clearTimeout(timeoutId);
+
+  return response;
+};
